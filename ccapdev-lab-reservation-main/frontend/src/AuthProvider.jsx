1f1f020ee6e58cf999144444280a4e4ec51f098e
aaaaandreiii <andrei.balingit.alab1@gmail.com>
2025-03-10 17:41:24 +0800

    JSON.parse -> JSON.stringify error in auth

--- ccapdev-lab-reservation-main/frontend/src/AuthProvider.jsx
+++ ccapdev-lab-reservation-main/frontend/src/AuthProvider.jsx
@@ -5,7 +5,7 @@ const AuthContext = createContext();
 
 const AuthProvider = ({children}) => {
     const [user, setUser] = useState(() => {
-        return JSON.parse(localStorage.getItem("user")) ?? null;
+        return JSON.stringify(localStorage.getItem("user")) ?? null;
     });
 
     const [errorMessage, setErrorMessage] = useState("");
